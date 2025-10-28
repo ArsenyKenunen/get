7655402ba6b8f6ceb6b9e373ec7cb02669ce969b
Arseny Kenunen <arsenykenunen@gmail.com>
2025-10-28 17:13:28 +0300

    minor changes

---
+++ get-adc/mcp3021_driver.py
@@ -0,0 +1,8 @@
+import smbus
+
+class MCP3021:
+    def __init__(self, dynamic_range, verbose = False):
+        self.bus = smbus.SMbus(1)
+        self.dynamic_range = dynamic_range
+        self.address = 0x4D
+        self.verbose = verbose
